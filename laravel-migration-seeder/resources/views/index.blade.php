2865ae794e3e24095297e0fca98c6a066d555a0e
camillapalombi <camilla.palombi14@gmail.com>
2022-05-05 17:37:57 +0200

    Stampa treni in partenza oggi

--- laravel-migration-seeder/resources/views/index.blade.php
+++ laravel-migration-seeder/resources/views/index.blade.php
@@ -5,25 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Trains</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
         <h1>Treni in partenza oggi:</h1>
     </header>
     <main>
+        <div class="container-all-cards">
 
         @foreach ($trains as $train)
                 <div class="single-card">
                     <ul>
                         <li> <h3> {{ $train->company }} </h3> </li>
-                        <li> <h4> Partenza: {{ $train->departureStation }} </h4> </li>
-                        <li> Arrivo: {{ $train->arrivalStation }}</li>
-                        <li> Orario partenza: {{ $train->departureTime }}</li>
-                        <li> Orario arrivo: {{ $train->arrivalTime }}</li>
                         <li> {{ $train->trainCode }}</li>
+                        <li> <h4>Partenza:</h4> {{ $train->departureStation }} </li>
+                        <li> <h4>Arrivo:</h4> {{ $train->arrivalStation }}</li>
+                        <li> <h4>Orario Partenza:</h4> {{ $train->departureTime }}</li>
+                        <li> <h4>Orario Arrivo:</h4> {{ $train->arrivalTime }}</li>
+                        
                     </ul>
                 </div>
             @endforeach
+
+        </div>
 
 
 
